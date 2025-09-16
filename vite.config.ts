6ee0c86eac6af431160363363aa0501f404c464e
Festim Beqiri <festimi-rrezoni@hotmail.com>
2025-09-16 08:24:51 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // listen on all interfaces
+    port: 8089,         // serve on port 80
+    strictPort: true  // fail instead of picking another port if 80 is busy
+  },
 })
